bcfd5058fe3dbd46bb813967af766eebf6f31904
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-01-24 23:19:49 +0900

    have to make form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component{ // React.component로 부터 상속 받는다.
   // mount는 application을 실행하면 생거나는것
@@ -7,16 +9,30 @@ class App extends React.Component{ // React.component로 부터 상속 받는다
     movies: []
   }; 
   // Q1 render을 하면 호출되는 life cycle method는? = componentDidMount
+  getMovies = async() => {
+    const {data: { data : {movies}}} = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false })
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000); // timeout: JS
+    this.getMovies();
   }
   render(){
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>{isLoading ? "Loading..." : "We are ready"}</div>
-    )
+      <div>{isLoading ? "Loading..." : movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+          />
+        ))}
+      </div>
+    );
   };
 }
 
